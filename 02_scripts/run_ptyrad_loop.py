375b95cf2ed8ad7f8a39eddab82dfe8344ec1a87
chiahao3 <harrey810719@gmail.com>
2025-06-11 16:47:17 -0400

    Add defocus series params files

--- 02_scripts/run_ptyrad_loop.py
+++ 02_scripts/run_ptyrad_loop.py
@@ -19,44 +19,46 @@ if __name__ == "__main__":
     parser.add_argument("--jobid", type=int, required=False, default=0, help="Unique identifier for hypertune mode with multiple GPU workers")
     args = parser.parse_args()
     
-    for round_idx in range(1, 6):
-        for batch in [1024, 512, 256, 128, 64, 32, 16]:
-            for pmode in [1, 3, 6, 12]:
-                for slice in [1, 3, 6]:
-                    try:
-                        # Setup CustomLogger
-                        logger = CustomLogger(
-                            log_file='ptyrad_log.txt',
-                            log_dir='auto',
-                            prefix_date=True,
-                            prefix_jobid=args.jobid,
-                            append_to_file=True,
-                            show_timestamp=True
-                        )            
-                        # Set up accelerator for multiGPU/mixed-precision setting, note that thess has no effect when we launch it with just `python <script>`
-                        accelerator = set_accelerator()
+    for round_idx in [1]:
+        for batch in [16]:
+            for pmode in [12]:
+                for slice in [6]:
+                    for df in [-120, -140, -160, -180, -200]: #[-20, -15, -10, -5, 0, 5, 10, 15, 20]:
+                        try:
+                            # Setup CustomLogger
+                            logger = CustomLogger(
+                                log_file='ptyrad_log.txt',
+                                log_dir='auto',
+                                prefix_date=True,
+                                prefix_jobid=args.jobid,
+                                append_to_file=True,
+                                show_timestamp=True
+                            )            
+                            # Set up accelerator for multiGPU/mixed-precision setting, note that thess has no effect when we launch it with just `python <script>`
+                            accelerator = set_accelerator()
+                                
+                            print_system_info()
+                            params = load_params(args.params_path)
+                            device = set_gpu_device(args.gpuid)
+                                            
+                            # Run ptyrad_ptycho_solver
+                            vprint(f"Running (round_idx, batch, pmode, slice, df) = {(round_idx, batch, pmode, slice, df)}")
                             
-                        print_system_info()
-                        params = load_params(args.params_path)
-                        device = set_gpu_device(args.gpuid)
-                                        
-                        # Run ptyrad_ptycho_solver
-                        vprint(f"Running (round_idx, batch, pmode, slice) = {(round_idx, batch, pmode, slice)}")
-                        
-                        params['recon_params']['output_dir'] += f'_r{str(round_idx)}/'
-                        params['recon_params']['BATCH_SIZE']['size'] = batch
-                        params['init_params']['probe_pmode_max'] = pmode
-                        params['init_params']['obj_Nlayer'] = slice
-                        params['init_params']['obj_slice_thickness'] = round(12/slice, 2)
-                        
-                        ptycho_solver = PtyRADSolver(params, device=device, acc=accelerator, logger=logger)
-                        ptycho_solver.run()
-                        
-                    except Exception as e:
-                        vprint(f"An error occurred for (round, batch, pmode, slice) = {(round_idx, batch, pmode, slice)}: {e}")
+                            # params['recon_params']['output_dir'] += f'_r{str(round_idx)}/'
+                            params['recon_params']['BATCH_SIZE']['size'] = batch
+                            params['init_params']['probe_pmode_max'] = pmode
+                            params['init_params']['obj_Nlayer'] = slice
+                            params['init_params']['obj_slice_thickness'] = round(12/slice, 2)
+                            params['init_params']['probe_defocus'] = df
+                            
+                            ptycho_solver = PtyRADSolver(params, device=device, acc=accelerator, logger=logger)
+                            ptycho_solver.run()
+                            
+                        except Exception as e:
+                            vprint(f"An error occurred for (round, batch, pmode, slice, df) = {(round_idx, batch, pmode, slice, df)}: {e}")
 
-                    finally:
-                        del ptycho_solver  # Clear model
-                        torch.cuda.empty_cache()
-                        gc.collect()
+                        finally:
+                            del ptycho_solver  # Clear model
+                            torch.cuda.empty_cache()
+                            gc.collect()
 
